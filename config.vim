e74b7c8547632be4a4f806dc00515dfde5a02292
erifranck-nunez-ch <erickfrank@goldmediatech.com>
2021-02-08 11:15:14 -0500

    add new configuration for space vim and instalation

---
+++ config.vim
@@ -0,0 +1,10 @@
+func! config#before() abort
+    let g:mapleader  = ','
+    " after this line, when you using <leader> to defind key bindings
+    " the leader is ;  for example:
+    nnoremap <leader>w :w<cr> " this mapping means using `;w` to save current file.
+endf
+func! config#after() abort
+    nnoremap <Leader>gf :FZF<CR>
+    nnoremap <F2>:tabNext<CR>
+endf
---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Source: https://gist.github.com/davejamesmiller/1965569
+ask() {
+  while true; do
+    if [ "${2:-}" = "Y" ]; then
+      prompt="Y/n"
+      default=Y
+    elif [ "${2:-}" = "N" ]; then
+      prompt="y/N"
+      default=N
+    else
+      prompt="y/n"
+      default=
+    fi
+    read -p "$1 [$prompt] " REPLY </dev/tty
+    if [ -z "$REPLY" ]; then
+      REPLY=$default
+    fi
+    case "$REPLY" in
+      Y*|y*) return 0 ;;
+      N*|n*) return 1 ;;
+    esac
+  done
+}
+
+dir=`pwd`
+
+if ask "install spaceVim ?" Y; then
+  curl -sLf https://spacevim.org/install.sh | bash
+  ln -sf ${dir}/init.toml ${HOME}/.SpaceVim.d/init.toml
+  ln -sf ${dir}/config.vim ${HOME}/.SpaceVim/autoload/config.vim
+fi
